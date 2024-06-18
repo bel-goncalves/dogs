b35e57cd0bf413f9e7d155fe14feaf226ca044a8
Isabel <isabel.glima9@gmail.com>
2024-06-18 17:35:28 -0300

    header inicio

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import React from "react";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React>
-    <App />
-  </React>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
