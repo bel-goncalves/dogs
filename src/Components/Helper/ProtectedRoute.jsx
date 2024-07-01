14dcd344cf338a36544b74f7baeb57b43616534a
bel-goncalves <igl@cesar.school>
2024-07-01 10:20:50 -0300

    protected route

---
+++ src/Components/Helper/ProtectedRoute.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { UserContext } from "../../UserContext";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const { login } = React.useContext(UserContext);
+  //   return login ? children : <Navigate to="/login" />;
+  if (login === true) {
+    return children;
+  } else if (login === false) {
+    return <Navigate to="/login" />;
+  } else {
+    //null = estagio inicial, carregando
+    return <></>;
+  }
+};
+
+export default ProtectedRoute;
