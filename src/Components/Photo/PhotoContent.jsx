e35bfa492a2a8492968b4bfc617c9e29a35bd715
bel-goncalves <igl@cesar.school>
2024-07-03 16:09:35 -0300

    feed modal

---
+++ src/Components/Photo/PhotoContent.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./PhotoContent.module.css";
+import { Link } from "react-router-dom";
+import PhotoComments from "./PhotoComments";
+
+const PhotoContent = ({ data }) => {
+  //data = object com photo, comments
+  const { photo, comments } = data;
+
+  return (
+    <div className={styles.photo}>
+      <div className={styles.img}>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.details}>
+        <p className={styles.author}>
+          <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+          <span className={styles.visualizacoes}>{photo.acessos}</span>
+        </p>
+        <h1 className="title">
+          <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+        </h1>
+        <ul className={styles.attributes}>
+          <li>{photo.peso} kg</li>
+          {photo.idade !== 1 ? (
+            <li>{photo.idade} anos</li>
+          ) : (
+            <li>{photo.idade} ano</li>
+          )}
+        </ul>
+      </div>
+      <PhotoComments id={photo.id} comments={photo.comments} />
+    </div>
+  );
+};
+
+export default PhotoContent;
