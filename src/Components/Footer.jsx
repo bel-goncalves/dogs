e8a3540696de3acee718f6fb7a5330c29fe4e81c
bel-goncalves <igl@cesar.school>
2024-07-10 18:35:21 -0300

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import DogFooter from "../Assets/dogs-footer.svg?react";
 
 const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <DogFooter />
+      <p>Dogs. Alguns direitos reservados.</p>
+    </div>
+  );
 };
 
 export default Footer;
